1009cd49482504cf83be8edb26eed6c276a053e5
alexR-0123 <47568@rws.uk.net>
2026-09-17 12:57:51 +0100

    Add Queue class with basic operations
    Implemented Queue class with enqueue, dequeue, and peek methods.
    Just some inputs need to be added towards the end so that the code can run

--- Week_02_GCSE_Basics/03_queue_simulation.py
+++ Week_02_GCSE_Basics/03_queue_simulation.py
@@ -10,7 +10,36 @@ TODO:
 - Fill in functions
 - Add demonstration code under `if __name__ == "__main__":`
 """
-
+class Queue:
+    def __init__(self,maxItems):
+        self.tiems = [None]*maxItems
+        self.front = 0
+        self.rear = -1
+        self.size = 0
+        self.maxSize = maxItems
+        
+    def enQueue(self, item):
+        if self.size == maxSize:
+            print("Queue is full, size =", self.size)
+        else:
+            self.rear(self.rear + 1)%(self.maxSize)
+            self.size += 1
+            self.items[self.rear] = item
+           
+    def deQueue(self):
+        if (self.size == 0):
+            return "Queue is empty"
+        else:
+            first = self.items[self.front]
+            self.size +=1
+            self.front(self.front + 1)%(self.maxSize)
+            return first
+        
+    def peek(self):
+        if self.size == 0:
+            return "Queue is empty "
+        else:
+            return self.items[self.front]
 def main():
     # TODO: Write demonstration/testing code
     # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
