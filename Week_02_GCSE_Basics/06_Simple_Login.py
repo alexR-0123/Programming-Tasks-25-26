02dec6f74d34ab3bcacb5963eee6f5c41ddcf810
alexR-0123 <47568@rws.uk.net>
2026-09-17 21:40:52 +0100

    Add simple login logic with access control
    Implement basic login functionality with username and password validation.

--- Week_02_GCSE_Basics/06_Simple_Login.py
+++ Week_02_GCSE_Basics/06_Simple_Login.py
@@ -11,6 +11,22 @@ TODO:
 - Fill in functions
 - Add demonstration code under `if __name__ == "__main__":`
 """
+print("Enter username and password for access")
+username = ""
+password = ""
+count = 0  
+while username != "iplaygtaVI" and password != "iplayGTAVI12345" and count < 3:
+        username = input("Enter username ")
+        password = input("Enter password ")
+        
+        if username == "iplaygtaVI" and password == "iplayGTAVI12345":
+            print("Access granted. You may enter now")
+            break
+        else:
+            print("Access denied. Think again")
+            count = count + 1
+
+
 
 def main():
     # TODO: Write demonstration/testing code
