2107e0a54a586648757eae035e0491a153daff84
alexR-0123 <47568@rws.uk.net>
2026-09-17 20:29:48 +0100

    Implement shopping list input and modification
    There is a problem when inputting data, you can only add items but when you type in "DONE", the program does not break. I could not understand from where this logic error is occurring from

--- Week_02_GCSE_Basics/05_Shopping_List.py
+++ Week_02_GCSE_Basics/05_Shopping_List.py
@@ -10,7 +10,29 @@ TODO:
 - Fill in functions
 - Add demonstration code under `if __name__ == "__main__":`
 """
+shopping_list = []
+print("Enter your shopping list")
+print("Type DONE when you are finished. \n")
 
+while True:
+    item = input("Add an item")
+    if item.upper() == "DONE":
+        break
+    shopping_list.append(item)
+    
+print("\nYour shopping list is: ")
+for i in range(len(shopping_list)):
+    print(f"{i+1}.{shopping_list[i]}")
+    
+change_item = input("\Would you like to change an item? (yes/no)").lower()
+if change_item == "yes":
+    item_num = int(input("Enter the number of the item"))
+    changed_item = input("Enter the item you would like to add instead")
+    shopping_list[item_num - 1] = chnaged_item
+        
+print("\nYour shopping list: ")
+for i in range(len(shopping_list)):
+        print(f"{i+1}. {shopping_list[i]}")
 def main():
     # TODO: Write demonstration/testing code
     # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
