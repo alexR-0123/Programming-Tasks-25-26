f64f7e4198a1eb1cfac6a8e6ecaaefdad66191e9
alexR-0123 <47568@rws.uk.net>
2026-09-17 12:30:39 +0100

    Add times_table function for multiplication tables
    Implemented recursive times_table function and added input handling.

--- Week_02_GCSE_Basics/02_Times_Tables.py
+++ Week_02_GCSE_Basics/02_Times_Tables.py
@@ -10,13 +10,18 @@ TODO:
 - Fill in functions
 - Add demonstration code under `if __name__ == "__main__":`
 """
-
+def times_table(n, i=1):
+    if i == 11:
+        return
+    print(n, "*", i, "=", n * i)
+    i = i + 1
+    times_table(n, i)
+if __name__ == "__main__":
+    n = int(input("Enter a number less than 13"))
+times_table(n)
 def main():
     # TODO: Write demonstration/testing code
     # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
     # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
     pass
 
-
-if __name__ == "__main__":
-    main()
